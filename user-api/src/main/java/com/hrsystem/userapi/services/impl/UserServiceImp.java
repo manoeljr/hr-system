136143ffb74e3cdec3dc797549e686f5a837878c
manoel.junior <manoel.junior@tceal.tc.br>
2023-01-05 20:30:37 -0300

    Iniciando o Eureka Server

--- user-api/src/main/java/com/hrsystem/userapi/services/impl/UserServiceImp.java
+++ user-api/src/main/java/com/hrsystem/userapi/services/impl/UserServiceImp.java
@@ -5,18 +5,23 @@ import com.hrsystem.userapi.repository.UserRepository;
 import com.hrsystem.userapi.services.UserService;
 import com.hrsystem.userapi.services.exceptions.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class UserServiceImp implements UserService {
 
     private final UserRepository userRepository;
+    private final Environment env;
 
     @Override
     public User findById(Long id) {
+        log.info("USER_SERVICE ::: Get request on " + env.getProperty("local.server.port") + " port");
         return userRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("User not found."));
     }
 
